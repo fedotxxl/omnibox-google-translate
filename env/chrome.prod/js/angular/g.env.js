af17018914042b6d29549484f06b860cdeb2fee9
fedor.belov <fbelov@gramant.ru>
2014-12-25 22:46:33 +0300

    correct rate urls

--- env/chrome.prod/js/angular/g.env.js
+++ env/chrome.prod/js/angular/g.env.js
@@ -5,8 +5,8 @@ angular.module('common').
     constant('_browser', 'chrome').
     factory('_privateRouter', function() {
         return {
-            rateItUrl: "https://chrome.google.com/webstore/detail/cekkckdbbkdmpfbfjpohidmenfccifif/reviews?utm_source=chrome-ntp-icon",
-            shareUrl: "http://bit.ly/1xeHKGA",
+            rateItUrl: "https://chrome.google.com/webstore/detail/cfmkdfaolkbgplikiheobklldmdccofo/reviews?utm_source=chrome-ntp-icon",
+            shareUrl: "http://bit.ly/1JS9dl1",
             hashMemPluginUrl: "https://chrome.google.com/webstore/detail/nmkgaalbomjiafnenbknaoeilejnhnce",
             yaDictionaryPluginUrl: "https://chrome.google.com/webstore/detail/cekkckdbbkdmpfbfjpohidmenfccifif"
         }
